239152dd5fb8ce9989a41a25e43c94d0862918fe
Carlos Tomé <carlostome1990@gmail.com>
2014-04-08 18:18:49 +0200

    added examples

--- shrdlite.js
+++ shrdlite.js
@@ -3,7 +3,7 @@
 var AjaxScript = "cgi-bin/ajaxwrapper.py";
 
 // List of the JSON files that contain example worlds:
-var ExampleNames = ["small","medium","basic1","basic2"];
+var ExampleNames = ["small","medium","basic1","basic2","complex","impossible"];
 var ExamplesFolder = "examples";
 
 // What the system says when it has nothing to do:
