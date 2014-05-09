0a5888d00a5e44db2d3c1ff82cd24d19dfb50ea8
Carlos Tomé <carlostome1990@gmail.com>
2014-05-09 16:17:08 +0200

    Fix typo

--- haskell/Suggestions.hs
+++ haskell/Suggestions.hs
@@ -49,8 +49,8 @@ getObjectDescription :: WorldState -> Id -> [String]
 getObjectDescription worldState id
    | id == "Floor" = ["the", "floor"]
    | otherwise =
-     let Just obj = M.lookup id objects
-         (Object size color form) = fewestAttributesToIdentifyObject objects obj world
+     let Just obj = M.lookup id (_objectsInfo worldState)
+         (Object size color form) = fewestAttributesToIdentifyObject worldState obj 
 
      in  map (map toLower) [show size,show color, show form]
 
